ce36f2626457f9481580089a9848193336609462
Marius Melzer <marius@rasumi.net>
2014-08-14 19:42:34 +0200

    first working version

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
 elixir-elliptic-curve
 =====================
 
-An elixir module for elliptic curve cryptography
+An elixir module for elliptic curve cryptography (MIT licence). You can use it to sign messages and to verify signatures with a public key.
+
+# Use
+
+Use an existing elliptic curve public key pair or generate one using openssl (adapt the curve name according to your needs):
+
+```
+openssl ecparam -out ec_private_key.pem -name secp521r1 -genkey
+openssl ec -in ec_private_key.pem -pubout -out ec_public_key.pem
+```
+
+Copy the ECC-Module-File into your project. It's a GenServer-Module, you can start a new process passing in both the private and the public key combined in one (still) pem-style string:
+
+```elixir
+pem_public = File.read! "ec_public_key.pem"
+pem_private = File.read! "ec_private_key.pem"
+pem = Enum.join [pem_public, pem_private]
+
+{:ok, pid} = GenServer.start_link ECC, [pem]
+{:ok, signature} = GenServer.call pid, {:sign, "Hello", :sha512}
+
+{:ok, public_key} = GenServer.call pid, :get_public_key
+{:ok, result} = GenServer.call pid, {:verify_signature, "Hello", signature, public_key, :sha512}
+IO.puts "Hello == Hello? #{result}" # true
+
+{:ok, result} = GenServer.call pid, {:verify_signature, "World", signature, public_key, :sha512}
+IO.puts "Hello == World? #{result}" # false
+```
---
+++ ecc.ex
@@ -0,0 +1,77 @@
+defmodule ECC do
+  use GenServer
+
+  def init([pem]) do
+    {:ok, %{
+        public: ECC.Crypto.parse_public_key(pem),
+        private: ECC.Crypto.parse_private_key(pem)
+    }}
+  end
+
+  def handle_call(:get_public_key, _from, keys) do
+    if keys.public do
+      {:reply, {:ok, keys.public}, keys}
+    else
+      {:reply, {:error, :no_public_key}, keys}
+    end
+  end
+
+  def handle_call({:sign, msg, hash_type}, _from, keys) do
+    {:reply, {:ok, ECC.Crypto.sign(msg, hash_type, keys.private)}, keys}
+  end
+
+  def handle_call({:verify_signature, msg, signature, public_key, hash_type}, _from, keys) do
+    result = ECC.Crypto.verify_signature(msg, signature, hash_type, public_key)
+    {:reply, {:ok, result}, keys}
+  end
+end
+
+defmodule ECC.Crypto do
+  def parse_public_key(pem) do
+    try do
+      pem_keys = :public_key.pem_decode(pem)
+
+      ec_params =
+        Enum.find(pem_keys, fn(k) -> elem(k,0) == :OTPEcpkParameters end)
+        |> put_elem(0, :EcpkParameters)
+        |> :public_key.pem_entry_decode
+
+      pem_public =
+        Enum.find(pem_keys, fn(k) -> elem(k,0) == :SubjectPublicKeyInfo end)
+        |> elem(1)
+      ec_point = :public_key.der_decode(:SubjectPublicKeyInfo, pem_public)
+        |> elem(2)
+        |> elem(1)
+
+      {{:ECPoint, ec_point}, ec_params}
+    rescue
+      _ -> nil
+    end
+  end
+
+  def parse_private_key(pem) do
+    try do
+      :public_key.pem_decode(pem)
+      |> Enum.find(fn(k) -> elem(k,0) == :ECPrivateKey end)
+      |> :public_key.pem_entry_decode
+    rescue
+      _ -> nil
+    end
+  end
+
+  def sign(msg, hash_type, private_key) do
+    try do
+      :public_key.sign msg, hash_type, private_key
+    rescue
+      _ -> nil
+    end
+  end
+
+  def verify_signature(msg, signature, hash_type, public_key) do
+    try do
+      :public_key.verify(msg, hash_type, signature, public_key)
+    rescue
+      _ -> nil
+    end
+  end
+end
