e2a6443ae9ef2d663dc5806b242d13b2fa7f576d
Marius Melzer <marius@rasumi.net>
2014-08-15 18:07:36 +0200

    add application name to ecc

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule ElixirEllipticCurve.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :elixir_elliptic_curve,
+    [app: :ecc,
      version: "0.1.0",
      elixir: "~> 0.15.0",
      description: description,
