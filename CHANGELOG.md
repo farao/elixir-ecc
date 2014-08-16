29d3c17e7007aed318183274988aa540b3088a01
Marius Melzer <marius@rasumi.net>
2014-08-16 14:51:28 +0200

    add travis and loosen elixir requirement to 0.14

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+## 0.1.1
+
+Also support Elixir v0.14
+
+## 0.1.0
+
+Initial Release
--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule ElixirEllipticCurve.Mixfile do
 
   def project do
     [app: :ecc,
-     version: "0.1.0",
-     elixir: "~> 0.15.0",
+     version: "0.1.1",
+     elixir: "~> 0.14",
      description: description,
      package: package,
      deps: deps]
