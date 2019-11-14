ecb71d8ab4dd515aaa8becdd44b67696f4c42e1d
Paul Götze <paul.christoph.goetze@gmail.com>
2019-11-14 17:55:45 +0100

    Autoformat test code using 'mix format'

--- test/ecc_server_test.exs
+++ test/ecc_server_test.exs
@@ -2,18 +2,23 @@ defmodule ECC.ServerTest do
   use ExUnit.Case
 
   test "signing/checking signatures" do
-    pem_public = File.read! "ec_public_key.pem"
-    pem_private = File.read! "ec_private_key.pem"
-    pem = Enum.join [pem_public, pem_private]
+    pem_public = File.read!("ec_public_key.pem")
+    pem_private = File.read!("ec_private_key.pem")
+    pem = Enum.join([pem_public, pem_private])
 
-    {:ok, _} = ECC.start_link pem, :ecc
-    {:ok, signature} = GenServer.call :ecc, {:sign, "Hello", :sha512}
+    {:ok, _} = ECC.start_link(pem, :ecc)
+    {:ok, signature} = GenServer.call(:ecc, {:sign, "Hello", :sha512})
 
-    {:ok, public_key} = GenServer.call :ecc, :get_public_key
-    {:ok, result} = GenServer.call :ecc, {:verify_signature, "Hello", signature, public_key, :sha512}
+    {:ok, public_key} = GenServer.call(:ecc, :get_public_key)
+
+    {:ok, result} =
+      GenServer.call(:ecc, {:verify_signature, "Hello", signature, public_key, :sha512})
+
     assert result
 
-    {:ok, result} = GenServer.call :ecc, {:verify_signature, "World", signature, public_key, :sha512}
+    {:ok, result} =
+      GenServer.call(:ecc, {:verify_signature, "World", signature, public_key, :sha512})
+
     assert not result
   end
 end
